adf5f9c87b2c465874d42879f90f22ca515f6675
Katherine Silvestre <katherinesilv@gmail.com>
2025-11-05 10:58:06 +0000

    atualização do field - error 500 fixed

--- models.py
+++ models.py
@@ -1,10 +1,10 @@
 from pydantic import BaseModel,Field
 
 class LivroBase(BaseModel):
-    isbn: str
-    titulo: str = Field(..., title="Título do Livro", description="Nome completo do livro", min_length=3)
-    autor: str = Field(..., title="Autor", description="Nome completo do autor")
-    ano_publicacao: int = Field(..., ge=1900, le=2025, description="Ano em que o livro foi publicado")
+    isbn: str = Field(None, title="ISBN", description="Código ISBN do livro")
+    titulo: str = Field( title="Título do Livro", description="Nome completo do livro", min_length=3)
+    autor: str = Field( title="Autor", description="Nome completo do autor")
+    ano_publicacao: int = Field( ge=1900, le=2025, description="Ano em que o livro foi publicado")
 
 
 class LivroCriar(LivroBase):
