718afa9521bb3aef8c53581246e2152e5a77c73b
Katherine Silvestre <katherinesilv@gmail.com>
2025-11-05 11:01:09 +0000

    date time added no BaseModel

--- models.py
+++ models.py
@@ -1,10 +1,13 @@
 from pydantic import BaseModel,Field
+from datetime import datetime
+
+ano_atual= datetime.now().year
 
 class LivroBase(BaseModel):
     isbn: str = Field(None, title="ISBN", description="Código ISBN do livro")
     titulo: str = Field( title="Título do Livro", description="Nome completo do livro", min_length=3)
     autor: str = Field( title="Autor", description="Nome completo do autor")
-    ano_publicacao: int = Field( ge=1900, le=2025, description="Ano em que o livro foi publicado")
+    ano_publicacao: int = Field( ge=1900, le=ano_atual, description="Ano em que o livro foi publicado")
 
 
 class LivroCriar(LivroBase):
