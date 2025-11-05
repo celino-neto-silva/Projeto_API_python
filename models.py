8aa370af2de54db2d40f347cea8a7ed2e2bb3c83
HenriqueLondon <43453306+HenriqueLondon@users.noreply.github.com>
2025-11-05 14:56:46 +0000

    Add LivroRespostaSemId model to models.py
    Added LivroRespostaSemId model without the 'id' field.

--- models.py
+++ models.py
@@ -3,29 +3,25 @@ from datetime import datetime
 
 ano_atual = datetime.now().year
 
-
 class LivroBase(BaseModel):
-    id: int
     isbn: str = Field(None, title="ISBN", description="Código ISBN do livro")
-    titulo: str = Field(title="Título do Livro",
-                        description="Nome completo do livro", min_length=3)
-    autor: str = Field(
-        title="Autor", description="Nome completo do autor", min_length=3)
-    ano_publicacao: int = Field(
-        ge=1900, le=ano_atual, description="Ano em que o livro foi publicado")
-
+    titulo: str = Field(title="Título do Livro", description="Nome completo do livro", min_length=3)
+    autor: str = Field(title="Autor", description="Nome completo do autor", min_length=3)
+    ano_publicacao: int = Field(ge=1900, le=ano_atual, description="Ano em que o livro foi publicado")
 
 class LivroCriar(LivroBase):
     pass
 
-
 class LivroAtualizar(LivroBase):
     pass
 
-
 class LivroResposta(LivroBase):
-    pass
+    id: int
 
     class Config:
+        from_attributes = True
 
+# Novo modelo sem o campo `id`
+class LivroRespostaSemId(LivroBase):
+    class Config:
         from_attributes = True
